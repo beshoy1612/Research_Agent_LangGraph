1b45e7ec01d849bb52db07e47fe19c13472b461f
beshoy_karam <beshoy1612.karam@gmail.com>
2026-09-17 17:20:53 +0300

    creating object

---
+++ src/utils/models.py
@@ -0,0 +1,6 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite" , temperature=0)
---
+++ src/utils/objects.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel,Field
+from typing import List
+
+class Analyst(BaseModel):
+    affiliation: str = Field(description="Primary affiliation of the analyst")
+    name: str = Field(description="Name of analyst")
+    role: str = Field(description="Role of the analyst in the context of the topic")
+    description: str = Field(description="Description of the analyst focus , concerns")  
+
+    @property
+    def persona(self)->str:
+        return f"Name: {self.name}\n Role: {self.role}\n Description :{self.description}\n Affiliation: {self.affiliation}"
