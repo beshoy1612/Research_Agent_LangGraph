b1ca955533670884b377ec77b62a529762f13336
beshoy_karam <beshoy1612.karam@gmail.com>
2026-09-18 15:31:24 +0300

    addding conditional edge between creating_analysis node & human feed_back

---
+++ src/utils/edges.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv 
+from .states import GenerateAnalystState
+from langgraph.graph import END
+from typing import Literal
+load_dotenv()
+
+#conditional edge
+def create_analysts_human_feedback_edges(state:GenerateAnalystState) -> Literal["create_analysts","END"]:
+    """return the next node to execute"""
+
+    human_analyst_feedback= state.get("human_analyst_feedback",None)
+
+    if human_analyst_feedback:
+        return "create_analysts"
+    
+    return END
